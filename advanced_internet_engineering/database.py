fc4a09ccf085d563d8153b90db6d682ccd819308
Piotr Tutak <p.tutak@outlook.com>
2020-04-06 23:24:10 +0200

    testing

--- advanced_internet_engineering/database.py
+++ advanced_internet_engineering/database.py
@@ -1,26 +1,51 @@
 import os.path
 import sqlite3
 
-from flask import current_app, g
+from flask import current_app
+from contextlib import contextmanager
 
 
 class Database:
     def __init__(self, path):
-        init = False
-
+        self._path = path
         if not os.path.isfile(path):
-            init = True
-
-        if "database" not in g:
-            g.database = sqlite3.connect(path, detect_types=sqlite3.PARSE_DECLTYPES)
-            g.database.row_factory = sqlite3.Row
-
-        self._database = g.database
-
-        if init:
             with current_app.open_resource("../database/schema.sql") as schema:
-                self._database.executescript(schema.read().decode())
+                with self._get_database() as database:
+                    database.executescript(schema.read().decode())
 
-    def close(self):
-        self._database.close()
-        g.pop("database")
+    @contextmanager
+    def _get_database(self):
+        database = sqlite3.connect(self._path, detect_types=sqlite3.PARSE_DECLTYPES)
+        database.row_factory = sqlite3.Row
+        yield database
+        database.close()
+
+    def _get_columns_values(self, value):
+        columns = tuple(sorted(value.keys()))
+        values = tuple(value[key] for key in columns)
+        return (columns, values)
+
+    def create(self, schema, value):
+        columns, values = self._get_columns_values(value)
+        columns = f"({','.join(columns)})"
+        values = (f'"{val}"' for val in values)
+        values = f"({','.join(values)})"
+        query_str = f"INSERT INTO {schema} {columns} VALUES {values}"
+        with self._get_database() as database:
+            cursor = database.cursor()
+            cursor.execute(query_str)
+            database.commit()
+
+    def read(self, schema, value):
+        if value is None:
+            query_str = f"SELECT * FROM {schema}"
+        else:
+            columns, _ = self._get_columns_values(value)
+            query = tuple(f"{col} = {val}" for col, val in value.items())
+            query = " AND ".join(query)
+            query_str = f"SELECT {','.join(columns)} FROM {schema} WHERE {query};"
+        with self._get_database() as database:
+            cursor = database.cursor()
+            for row in cursor.execute(query_str):
+                print(tuple(row))
+        return "0"
--- advanced_internet_engineering/routes.py
+++ advanced_internet_engineering/routes.py
@@ -1,8 +1,23 @@
-import os.path
-from flask import render_template, current_app
-from advanced_internet_engineering.app import app
+from flask import render_template, current_app, request
+from advanced_internet_engineering.app import app, database
 
 
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route("/<schema>/create", methods=["POST"])
+def schema_create(schema):
+    content = request.json
+    print(content)
+    database.create(schema, content)
+    return "1"
+
+@app.route("/<schema>/read", methods=["GET"])
+def schema_read(schema):
+    args = request.args
+    if args:
+        return database.read(schema, args)
+    else:
+        return database.read(schema, None)
